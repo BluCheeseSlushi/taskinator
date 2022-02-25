0bc47dc65fca647840bdbc9b7b7ca366888a2d7c
Josiah Douglass <josiahdouglass64@gmail.com>
2022-02-25 08:38:18 -0500

    Fixed append bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,12 @@
-// let buttonEl = document.querySelector("#save-task");
-// let tasksToDoEl = document.querySelector("#tasks-to-do");
-
-// buttonEl.addEventListener("click", function() {
-//   let listItemEl = document.createElement("li");
-//   listItemEl.className = "task-item";
-//   listItemEl.textContent = "This is a new task.";
-//   tasksToDoEl.appendChild(listItemEl);
-// });
-
-const buttonEl = document.querySelector("#save-task"); 
-const tasksToDoEl = document.querySelector("#tasks-to-do"); 
+let formEl = document.querySelector("#task-form");
+let tasksToDoEl = document.querySelector("#tasks-to-do"); 
+console.log(tasksToDoEl);
 
 let createTaskHandler = function() {
   let listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
-  }; 
+  }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
