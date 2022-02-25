2f652f95de07c9dea0597b2103e920773efe88a2
Josiah Douglass <josiahdouglass64@gmail.com>
2022-02-25 08:53:16 -0500

    improved form

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,15 @@ let formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#tasks-to-do"); 
 console.log(tasksToDoEl);
 
-let createTaskHandler = function() {
+let createTaskHandler = function(event) {
+
+  event.preventDefault();
+
   let listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
-  }
+}
+
 
 formEl.addEventListener("submit", createTaskHandler);
